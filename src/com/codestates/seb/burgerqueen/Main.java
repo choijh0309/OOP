b66997bebaa596cf1799d42559e3c1b437025ad5
JUNHA CHOI <choijh0309@naver.com>
2024-04-02 18:00:01 +0900

    oop commit

---
+++ src/com/codestates/seb/burgerqueen/Main.java
@@ -0,0 +1,18 @@
+package com.codestates.seb.burgerqueen;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        AppConfigurer appConfigurer = new AppConfigurer();
+
+        OrderApp orderApp = new OrderApp(
+                appConfigurer.productRepository(),
+                appConfigurer.menu(),
+                appConfigurer.cart(),
+                appConfigurer.order()
+        );
+
+        orderApp.start();
+    }
+}
